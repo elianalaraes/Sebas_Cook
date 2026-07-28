883728bdd3053f09b580b86ca8dcd944951ae7ce
ElianaLara <c3074085@newcastle.ac.uk>
2026-07-28 19:11:18 +0100

    Add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+
     from .routes import main
     app.register_blueprint(main)
+
+    with app.app_context():
+        from . import models
+        db.create_all()
 
     return app
