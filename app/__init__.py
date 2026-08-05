783b6dda2e3f9c0b1d190c2c570b861d97f80032
ElianaLara <c3074085@newcastle.ac.uk>
2026-08-05 22:45:43 +0100

    add login manager

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
+
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
+    login_manager.login_message = 'Por favor inicia sesión para acceder.'
+    login_manager.login_message_category = 'danger'
+
+
+    from app.models import Admin
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Admin.query.get(int(user_id))
 
     from .routes import main
     app.register_blueprint(main)
